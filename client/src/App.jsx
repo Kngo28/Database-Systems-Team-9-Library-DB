0cd49c89971078862c8220b68fb2e8893e993f76
ebrahim-vejlani <e1.vejlani@gmail.com>
2026-03-24 22:48:35 -0500

    add catalog page with search and browse functionality

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,7 @@
 import { Routes, Route, Navigate } from "react-router-dom";
 import LoginPage from "./pages/LoginPage";
 import RegisterPage from "./pages/RegisterPage";
+import CatalogPage from "./pages/CatalogPage";
 import CustomerPage from "./pages/CustomerPage";
 import StaffPage from "./pages/StaffPage";
 import ManageItemsPage from "./pages/ManageItemsPage";
@@ -16,6 +17,7 @@ export default function App() {
       <Route path="/" element={<Navigate to="/login" replace />} />
       <Route path="/login" element={<LoginPage />} />
       <Route path="/register" element={<RegisterPage />} />
+      <Route path="/catalog" element={<CatalogPage />} />
       <Route path="/customer" element={<CustomerPage />} />
       <Route path="/staff" element={<StaffPage />} />
       <Route path="/manage-items" element={<ManageItemsPage />} />
